017685a921099cf96f0214de1cd59a098350f497
Sebastien Chemin <sebastien.chemin@scmfrance.fr>
2016-07-18 13:47:31 +0200

    Add argument to run with autofix
    Enable the '-a' option from rubocop to fix issues automatically
    Just use this in the .pre-commit-config.yaml :
    ```
      hooks:
      - id: check-rubocop
        args: [-a]
    ```
    
    Change-Id: Idcddc6ae74437e2499344df9392742220ef98ab1

--- pre_commit_hooks/check_rubocop.py
+++ pre_commit_hooks/check_rubocop.py
@@ -7,12 +7,16 @@ import sys
 
 def check_rubocop(argv=None):
     parser = argparse.ArgumentParser()
+    parser.add_argument('-a', help='auto fix', action='store_true')
     parser.add_argument('filenames', nargs='*', help='filenames to check.')
     args = parser.parse_args(argv)
 
     retval = 0
 
     command = ["rubocop", "--force-exclusion", "--display-cop-names"] + args.filenames
+
+    if args.a == True:
+        command += ["-a"]
 
     try:
         retval = subprocess.check_call(command, shell=False)
